c0f3650e116075772c63e781c793a10cc58a29fb
abdollahi.mahdi1985 <abdollahi.mahdi1985@gmail.com>
2023-06-07 01:39:32 +0330

    login decorator

--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from . import models
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
 def articleslist(request):
-    articles = models.Articles.objects.all()
+    articles = models.Articles.objects.all().order_by('-date')
     args = {'articles': articles}
     return render(request, 'articles/articleslist.html', args)
 
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    article = models.Articles.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login")
+def create_article(request):
+    return render(request, 'articles/create_atricle.html')
--- blog_site/urls.py
+++ blog_site/urls.py
@@ -19,11 +19,15 @@ from django.urls import path, include
 # from blog import views
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', views.home_page, name='home'),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ]
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
